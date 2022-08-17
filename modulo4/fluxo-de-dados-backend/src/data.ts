bc45825c7fb68408e31df7d09590a1b997f6997d
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2022-08-17 16:30:27 -0300

    fluxo de dados backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,23 @@
+export type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const productList: Product[] = [
+    {
+        id: "1",
+        name: "book",
+        price: 12
+    },
+    {
+        id: "2",
+        name: "pen",
+        price: 2
+    },
+    {
+        id: "3",
+        name: "pencil",
+        price: 1
+    },
+]

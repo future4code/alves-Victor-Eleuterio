bd779c29c76797dfbd2667d496f37463938c0eb7
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2022-08-30 16:51:35 -0300

    Filtros, ordenação e paginação

--- modulo4/filtros-ordenacao-paginacao/src/types.ts
+++ modulo4/filtros-ordenacao-paginacao/src/types.ts
@@ -1,15 +1,12 @@
-export type recipe = {
-   id: string
-   title: string
-   description: string
-   userId: string
-   createdAt: number
-}
-
 export type user = {
-   id: string
-   name: string
-   email: string
-   password: string
-   recipes?: recipe[]
-}
+   id: number,
+   name: string,
+   email: string,
+   type: string
+};
+
+export enum TYPE {
+   OPERATIONS = "OPERATIONS",
+   TEACHER = "TEACHER",
+   CX = "CX"
+};

8ade7db88c588abc9b1a862dd864fece0e8a517a
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2022-08-30 16:50:31 -0300

    Filtros, ordenação e paginação

--- modulo4/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -1,4 +1,12 @@
 import { app } from "./app";
-import { getUsersFilterByName } from "./endpoints/getAllUsers";
+import { getAllUsersOrdered } from "./endpoints/getAllUsersOrdered";
+import { getUsersFilterByName } from "./endpoints/getUsersFilterByName";
+import { getUsersFilterByType } from './endpoints/getUsersFilterByType';
+import { getUsersByPages } from './endpoints/getUsersByPages';
+import { getUsersFilterAndOrderAndPage } from "./endpoints/getUsersFilterAndOrderAndPage";
 
-app.get("/user", getUsersFilterByName );
+app.get("/user", getUsersFilterByName);
+app.get("/user/fullsearch", getUsersFilterAndOrderAndPage);
+app.get("/user/search", getAllUsersOrdered);
+app.get("/user/page", getUsersByPages);
+app.get("/user/:type", getUsersFilterByType);
